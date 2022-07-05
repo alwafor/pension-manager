f4f883b3c0698d6cd51af01c96b7fa61a84e3fe7
Alwafor <alwafor1@gmail.com>
2022-07-05 16:58:53 +0300

    Add container style for app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,10 @@ import Navbar from '@/components/layout/navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className='container'>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </div>
   )
 }
 
